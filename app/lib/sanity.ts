145db326aca1bb2b485de0e490beffdd2d92053e
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-07-14 11:16:55 +0545

    added token in env

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,12 +1,15 @@
 import { createClient } from 'next-sanity';
 
 const projectId = process.env.Next_APP_SANITY_PROJECT_ID;
+const token = process.env.NEXT_APP_SANITY_TOKEN;
 const dataset = 'production';
 const apiVersion = '2023-01-01';
 
 export const client = createClient({
   projectId,
+  token,
   dataset,
   apiVersion,
   useCdn: true,
+  ignoreBrowserTokenWarning: true,
 });
